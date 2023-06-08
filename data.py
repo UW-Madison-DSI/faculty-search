c4e0ed122bead94fd0dd3a6ebd4e5b2d28b8d72b
Jason Lo <lcmjlo@gmail.com>
2023-06-08 20:45:31 +0000

    add get abstract function

--- data.py
+++ data.py
@@ -1,4 +1,6 @@
 import os
+import re
+import requests
 import pandas as pd
 
 
@@ -31,3 +33,57 @@ def extract_metadata(metadatas: list[dict], key: str) -> list:
     for metadata in metadatas:
         values.append(metadata[key])
     return values
+
+
+def get_abstract(doi: str) -> str | None:
+    """Get abstract from Crossref."""
+    api_url = f"https://api.crossref.org/works/{doi}"
+    response = requests.get(api_url)
+
+    if response.status_code == 200:
+        data = response.json()
+
+        if "abstract" not in data["message"]:
+            return None
+        return data["message"]["abstract"]
+    else:
+        return None
+
+
+# Basic abstract cleaning functions
+def strip_xml_tags(text: str) -> str:
+    return re.sub(r"<[^>]*>", " ", text)
+
+
+def strip_latex(text: str) -> str:
+    return re.sub(r"\$.*?\$", "", text)
+
+
+def remove_extra_spaces(text: str) -> str:
+    return re.sub(r"\s+", " ", text)
+
+
+def remove_leading_spaces(text: str) -> str:
+    return re.sub(r"^\s+", "", text)
+
+
+def strip_words_with_backslash(text: str) -> str:
+    return re.sub(r"\\[^ ]+", "", text)
+
+
+def strip_curly_braces(text: str) -> str:
+    return re.sub(r"\{.*?\}", "", text)
+
+
+def to_plain_text(text: str) -> str:
+    """Minimal text cleaning for JATS XML."""
+
+    if text is None:
+        return None
+
+    text = strip_xml_tags(text)
+    text = strip_latex(text)
+    text = remove_extra_spaces(text)
+    text = remove_leading_spaces(text)
+    text = strip_words_with_backslash(text)
+    return strip_curly_braces(text)
