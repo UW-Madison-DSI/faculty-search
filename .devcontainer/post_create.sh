8a73f6d2944c8bb4fd3cea4bbfacbe29c1834b0c
Jason Lo <lcmjlo@gmail.com>
2023-07-10 21:22:45 +0000

    try deploy on uw posit

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -3,3 +3,5 @@
 pip install --upgrade pip
 pip install -r .devcontainer/requirements.txt
 pip install -e .
+source .env
+rsconnect add --server https://data-viz.it.wisc.edu/ --name wisc --api-key $POSIT_API_KEY
