bcf219df6cae360475ef9917f10155b2bee15690
Jason Lo <lcmjlo@gmail.com>
2023-08-04 16:25:25 +0000

    add tests for core api functionality

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,20 +1,39 @@
 import pytest
+import os
 from pymilvus import Collection, connections, utility
 from langchain.embeddings import OpenAIEmbeddings
+from dotenv import load_dotenv
+
+load_dotenv()
+API_URL = os.getenv("API_URL", "http://localhost:8000")
+
 
 @pytest.fixture(scope="session", autouse=True)
 def connection():
     connections.connect("default", host="milvus-standalone", port="19530")
     print(utility.get_server_version())
 
+
 @pytest.fixture
 def author_collection():
     return Collection("authors")
+
 
 @pytest.fixture
 def article_collection():
     return Collection("articles")
 
+
 @pytest.fixture
 def embeddings():
     return OpenAIEmbeddings()
+
+
+@pytest.fixture
+def search_articles_route():
+    return f"{API_URL}/search_articles"
+
+
+@pytest.fixture
+def search_authors_route():
+    return f"{API_URL}/search_authors"
--- tests/test_api_engine.py
+++ tests/test_api_engine.py
@@ -1,14 +1,15 @@
 from api.core import *
 
+
 def test_sort_dict_by_value():
-    d = {'a': 3, 'b': 1, 'c': 2}
+    d = {"a": 3, "b": 1, "c": 2}
     sorted_keys, sorted_values = sort_dict_by_value(d)
-    assert sorted_keys == ['b', 'c', 'a']
+    assert sorted_keys == ["b", "c", "a"]
     assert sorted_values == [1, 2, 3]
 
-    d = {'a': 3, 'b': 1, 'c': 2}
+    d = {"a": 3, "b": 1, "c": 2}
     sorted_keys, sorted_values = sort_dict_by_value(d, reversed=True)
-    assert sorted_keys == ['a', 'c', 'b']
+    assert sorted_keys == ["a", "c", "b"]
     assert sorted_values == [3, 2, 1]
 
     d = {}
@@ -18,7 +19,6 @@ def test_sort_dict_by_value():
 
 
 def test_engine(author_collection, article_collection, embeddings):
-
     engine = Engine(author_collection, article_collection, embeddings)
 
     # Test the embed method
@@ -28,21 +28,23 @@ def test_engine(author_collection, article_collection, embeddings):
     assert len(embedded) == 1536
 
     # Test the get_author method
-    author = engine.get_author(106927)
+    author = get_author(106927, author_collection)
     assert isinstance(author, dict)
     assert author["id"] == 106927
     assert author["first_name"] == "Kyle"
     assert author["last_name"] == "Cranmer"
 
     # Test the search_articles method
-    articles = engine.search_articles("Dark Higgs Boson", top_k=3)
-    assert isinstance(articles, list)
+    results = engine.search_articles("Dark Higgs Boson", top_k=3)
+    assert isinstance(results, dict)
+    articles = results["articles"]
     assert len(articles) <= 3
     assert isinstance(articles[0], dict)
     assert list(articles[0].keys()) == ["distance", "title", "author_id", "doi"]
 
     # Test the search_authors method
-    author_ids, scores = engine.search_authors("Dark Higgs Boson", top_k=3)
+    authors = engine.search_authors("Dark Higgs Boson", top_k=3)["authors"]
+    author_ids, scores = authors["author_ids"], authors["scores"]
     assert isinstance(author_ids, list)
     assert isinstance(scores, list)
     assert len(author_ids) <= 3
