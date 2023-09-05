a6c8181dc523f2795d2cf1c1c02fa04899c6bb51
Jason Lo <lcmjlo@gmail.com>
2023-09-05 19:29:02 +0000

    add ssl key copying in deploy script

--- scripts/deploy_frontend.sh
+++ scripts/deploy_frontend.sh
@@ -1,6 +1,15 @@
 #! /bin/bash
+# This is suppose to be run on GCP 
 
 cd /community-search
 sudo su lcmjlo
+
+# Pull the latest image
 docker compose pull flask
+
+# Copy the certificates to frontend container
+docker compose cp cert.pem flask:/app
+docker compose cp privkey.pem flask:/app
+
+# Start the frontend container
 docker compose up -d --build
