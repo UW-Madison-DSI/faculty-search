425e77a0a27e8773cb996ea8f97a822b2a961cf9
Jason Lo <lcmjlo@gmail.com>
2023-09-12 21:44:48 +0000

    set new mail server

--- frontend/services/controllers/contact_controller.py
+++ frontend/services/controllers/contact_controller.py
@@ -27,8 +27,8 @@ class ContactController:
         app = flask.current_app
         app.config["MAIL_SERVER"] = os.getenv("MAIL_SERVER")
         app.config["MAIL_PORT"] = os.getenv("MAIL_PORT")
-        app.config["MAIL_USERNAME"] = os.getenv("MAIL_USERNAME")
-        app.config["MAIL_PASSWORD"] = os.getenv("MAIL_PASSWORD")
+        # app.config["MAIL_USERNAME"] = os.getenv("MAIL_USERNAME")
+        # app.config["MAIL_PASSWORD"] = os.getenv("MAIL_PASSWORD")
         app.config["MAIL_USE_TLS"] = os.getenv("MAIL_USE_TLS")
         app.config["MAIL_USE_SSL"] = os.getenv("MAIL_USE_SSL")
 
