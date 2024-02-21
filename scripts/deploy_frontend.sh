be97b9ff46447137576820c3f9b1945b7a32b000
Jason Lo <lcmjlo@gmail.com>
2024-02-21 22:16:48 +0000

    fix broken ci

--- scripts/deploy_frontend.sh
+++ scripts/deploy_frontend.sh
@@ -1,10 +1,12 @@
 #! /bin/bash
-# This is suppose to be run on GCP 
+# This is suppose to be run on GCP
 
 cd /faculty-search
 sudo su lcmjlo
 
 # Pull the latest image
+source .env
+echo $GHCR_TOKEN | docker login ghcr.io -u USERNAME --password-stdin
 docker compose down flask
 docker compose pull flask
 docker compose up flask --build -d
